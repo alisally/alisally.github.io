4ac72db9f93928d4a70b42886280c31c0895496d
alisally <35058609+alisally@users.noreply.github.com>
2018-01-06 13:44:57 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,28 @@
-importScripts('/__/firebase/3.8.0/firebase-app.js');
-importScripts('/__/firebase/3.8.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+﻿importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase.js');
 
-firebase.messaging();
+
+var config = {
+  apiKey: "AIzaSyD4_zMt8NByD4zsxQ8Ugw3NB9MrQN5vfiI",
+        authDomain: "beautycenter-bb072.firebaseapp.com",
+        databaseURL: "https://beautycenter-bb072.firebaseio.com",
+        projectId: "beautycenter-bb072",
+        storageBucket: "beautycenter-bb072.appspot.com",
+        messagingSenderId: "129581670274"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+}
