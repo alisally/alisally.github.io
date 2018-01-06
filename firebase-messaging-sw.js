fd63914510ff0c8c392ea228dd8bb24f438df678
alisally <35058609+alisally@users.noreply.github.com>
2018-01-06 15:24:45 +0300

    fdsafsafathgfsh

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,9 +2,12 @@
 importScripts('/__/firebase/3.8.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');*/
 
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-/*importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js');
+/*importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js');*/
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');*/
+
+
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase.js');
 firebase.messaging();
